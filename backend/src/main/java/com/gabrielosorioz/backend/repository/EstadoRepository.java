d9736c4134e57bbae69346bea61a55c3c187b8d8
Gabriel Osorio <gabrielosorio013@gmail.com>
2023-09-15 15:33:57 -0300

    adicionando back-crud-estado

---
+++ backend/src/main/java/com/gabrielosorioz/backend/repository/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielosorioz.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabrielosorioz.backend.entity.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Long>{
+    
+}
