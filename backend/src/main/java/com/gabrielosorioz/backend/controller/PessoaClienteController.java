351040fa09aefd3c3e64b92952829d05ea100f51
Gabriel Osorio <gabrielosorio013@gmail.com>
2023-09-18 11:37:36 -0300

    Adicionando o DTO cadastro-pessoa como cliente

---
+++ backend/src/main/java/com/gabrielosorioz/backend/controller/PessoaClienteController.java
@@ -0,0 +1,27 @@
+package com.gabrielosorioz.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabrielosorioz.backend.dto.PessoaClienteRequestDTO;
+import com.gabrielosorioz.backend.entity.Pessoa;
+import com.gabrielosorioz.backend.service.PessoaService;
+
+@RestController
+@RequestMapping("api/cliente")
+public class PessoaClienteController {
+    
+    @Autowired
+    private PessoaService pessoaService;
+
+     @PostMapping("/")
+     public Pessoa inserir (@RequestBody PessoaClienteRequestDTO pessoaClienteRequestDTO){    
+        Pessoa pessoa = new PessoaClienteRequestDTO().converter(pessoaClienteRequestDTO);
+        return pessoaService.inserir(pessoa);
+     
+    }
+
+}
