68ab84b9f3f971a2766c3eed48e0a3ebef89086b
Gabriel Osorio <gabrielosorio013@gmail.com>
2023-09-20 11:20:49 -0300

    Esta alteração introduz um envio de email template na classe '' PessoaClienteService.java
    O email é gerado com base nas informações fornecidas, incluindo a mensagem e o nome da pessoa.

--- backend/src/main/java/com/gabrielosorioz/backend/service/PessoaClienteService.java
+++ backend/src/main/java/com/gabrielosorioz/backend/service/PessoaClienteService.java
@@ -1,12 +1,15 @@
 package com.gabrielosorioz.backend.service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.gabrielosorioz.backend.dto.PessoaClienteRequestDTO;
 import com.gabrielosorioz.backend.entity.Pessoa;
 import com.gabrielosorioz.backend.repository.PessoaClienteRepository;
+
 
 
 @Service
@@ -22,14 +25,20 @@ public class PessoaClienteService {
     private EmailService emailService;
 
     private String mensagem = "O registro na loja foi realizado com Sucesso. Em breve você receberá a senha de acesso por e-mail" ;
+    String tituloEmail = "Cadastro";
 
     public Pessoa registrar(PessoaClienteRequestDTO pessoaClienteRequestDTO){
+        
         Pessoa pessoa = new PessoaClienteRequestDTO().converter(pessoaClienteRequestDTO);
         pessoa.setDataCriacao(new Date());
         Pessoa newPessoa = pessoaClienteRepository.saveAndFlush(pessoa);
         permissaoPessoaService.vincularPessoaPermissaoCliente(newPessoa);
-        emailService.enviarEmailTexto(newPessoa.getEmail(), "Cadastro na Loja MegaSource", mensagem);
+        Map<String,Object> propriedadesMap = new HashMap<>();
+        propriedadesMap.put("mensagem",mensagem);
+        propriedadesMap.put("nome", newPessoa.getNome());
+        emailService.enviarEmailTemplate(newPessoa.getEmail(), tituloEmail,propriedadesMap);
 
         return newPessoa;
+        
     }
-}
+} 
