a904f033ff84cd72d2f2a350eb1bb63d9333e842
Gabriel Osorio <gabrielosorio013@gmail.com>
2023-09-23 11:53:35 -0300

    fix: remove comentários desnecessários

--- backend/src/main/java/com/gabrielosorioz/backend/controller/EstadoController.java
+++ backend/src/main/java/com/gabrielosorioz/backend/controller/EstadoController.java
@@ -24,34 +24,29 @@ public class EstadoController {
 
     
     @GetMapping("/")
-    // @CrossOrigin("http://localhost:3000")
     public List<Estado> buscarTodos(){
         return estadoService.buscarTodos();
     }
 
     
     @PostMapping("/")
-    // @CrossOrigin("http://localhost:3000")
     public Estado inserir(@RequestBody Estado estado){
         return estadoService.inserir(estado);
     }
 
     @PutMapping("/")
-    // @CrossOrigin("http://localhost:3000")
     public Estado alterar(@RequestBody Estado estado){
         return estadoService.alterar(estado);
     }
 
     
     @DeleteMapping("/{id}")
-    // @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> excluir(@PathVariable ("id") Long id){
         estadoService.excluir(id);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/{id}")
-    // @CrossOrigin("http://localhost:3000")   
+    @GetMapping("/{id}")   
     public ResponseEntity<Estado> buscarPorId(@PathVariable("id") Long id){
         return ResponseEntity.ok(estadoService.buscarPorId(id));
     
